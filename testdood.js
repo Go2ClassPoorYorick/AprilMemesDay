e7032d41d19ece888d7ad5417d8446b4005218fb
Go2ClassPoorYorick <33301123+Go2ClassPoorYorick@users.noreply.github.com>
2017-11-01 21:04:07 -0400

    Update testdood.js

--- testdood.js
+++ testdood.js
@@ -10,8 +10,10 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
+  //compares the highest role of the author of a message to the rank chosen as a cut-off. If the author is above or equal to the chosen role, the function exits.
   var isAdmin = message.member.highestRole.comparePositionTo(message.guild.roles.get("roleID"));
   if (isAdmin >=0){return}
+  //if the author is not above the chosen role, the else function converts the message text to lowercase, and checks for "Text required" inside the content. if it isn't found, it deletes the message.
   else{
   var string = message.content;
   var stringLow = string.toLowerCase();
